fb7f3d1d434f8b28af32516d0a2bef927c0a002e
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-09 09:55:47 +0200

    add initial text state and method

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import Icon from 'react-native-ico-material-design';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const iconHeight = 26;
+const iconWidth = 26;
+
+export default class App extends React.Component {
+  state = {
+    screenText: 'Press a button!'
+  }
+
+    changeText = (text) => {
+      console.log(`${text} has been pressed`)
+      this.setState({
+        screenText: text
+      })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+              <View>
+                <Text style={{fontSize: 30, color: 'white'}}>{this.state.screenText}</Text>
+              </View>
+                <StatusBar style='light' />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#3962FF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
